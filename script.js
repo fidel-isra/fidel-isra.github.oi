c539d850e5f9d268c616314f157d44dbc3f1f675
fidel-isra <104939140+fidel-isra@users.noreply.github.com>
2024-09-27 23:38:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function abrirCarta() {
+    document.getElementById("carta").classList.add("abierta");
+}
+
+function cerrarCarta() {
+    document.getElementById("carta").classList.remove("abierta");
+}
